85cda46bbbe4cb4085067ba7ae9a50052d15d30b
Marcin Drozd <marcinx52@gmail.com>
2019-03-15 20:18:49 +0100

    Dodaj pliki projektów.

---
+++ LS-Shop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LS_Shop.MvcApplication" Language="C#" %>
---
+++ LS-Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LS_Shop.Startup))]
+namespace LS_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
