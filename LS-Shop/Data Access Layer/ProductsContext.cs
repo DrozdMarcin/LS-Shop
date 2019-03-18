8f5c255d4ce39f927f56d1efe951a7c739263d48
fabros <fabros@op.pl>
2019-03-18 22:30:57 +0100

    Dodanie ProductsContext do wygenerowania bazy - EF i podpęcie LocalDb

---
+++ LS-Shop/Data Access Layer/ProductsContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using LS_Shop.Models;
+
+namespace LS_Shop.Data_Access_Layer
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext() : base("ProductsContext")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderPosition> OrderPositions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // using System.Data.Entity.ModelConfiguration.Conventions;
+            // Wyłącza konwencję, która automatycznie tworzy liczbę mnogą dla nazw tabel w bazie danych
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
