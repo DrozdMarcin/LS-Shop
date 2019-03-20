75d095725979d961c78be92d0bd5e50e25c5339a
miloszkulesza <milosz.kulesza1@gmail.com>
2019-03-20 14:44:32 +0100

    Dodanie rozwijanej listy w navbarze po zalogowaniu

--- LS-Shop/Migrations/201903201232557_init.Designer.cs
+++ LS-Shop/Migrations/201903201232557_init.Designer.cs
@@ -7,13 +7,13 @@ namespace LS_Shop.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201903190947370_initial"; }
+            get { return "201903201232557_init"; }
         }
         
         string IMigrationMetadata.Source
