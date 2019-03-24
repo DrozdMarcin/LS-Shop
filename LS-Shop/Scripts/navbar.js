749e74476047fca13e7d4606c2c0d165f6edebc1
fabros <fabros@op.pl>
2019-03-24 14:18:26 +0100

    Wrzucenie js do właściwego folderu

---
+++ LS-Shop/Scripts/navbar.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $("#search").mouseenter(function () {
+        $("#search").css("background-color", "rgba(100, 120, 140, 0.5)");
+    });
+    $("#search").mouseleave(function () {
+        $("#search").css("background-color", "rgba(52, 58, 64, 0.5)");
+    });
+
+    $("#user").mouseenter(function () {
+        $("#user").css("color", "lightcyan");
+    });
+    $("#user").mouseleave(function () {
+        $("#user").css("color", "lightblue");
+    });
+
+    $("#cart").mouseenter(function () {
+        $("#cart").css("background-color", "rgba(52, 95, 115, 0.5)");
+    });
+    $("#cart").mouseleave(function () {
+        $("#cart").css("background-color", "rgba(52, 58, 64, 0.5)");
+    });
+
+    $("#ListItem").mouseenter(function () {
+        $("#ListItem").css("background-color", "#ddffff");
+    });
+    $("#ListItem").mouseleave(function () {
+        $("#ListItem").css("background-color", "lightgray");
+    });
+
+    $("#ListItem2").mouseenter(function () {
+        $("#ListItem2").css("background-color", "#ddffff");
+    });
+    $("#ListItem2").mouseleave(function () {
+        $("#ListItem2").css("background-color", "lightgray");
+    });
+});
