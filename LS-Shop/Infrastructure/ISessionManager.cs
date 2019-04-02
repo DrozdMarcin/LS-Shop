24db76a647c6f19cab7da62a0ec5f752c82b8d03
Marcin Drozd <marcinx52@gmail.com>
2019-04-02 21:24:55 +0200

    Utworzenie interfejsu i klasy sesji + dodanie klasy implementującej interfejs sesji, dodanie zmiennej sesji i zainstancjonowanie jej w konstruktorze

---
+++ LS-Shop/Infrastructure/ISessionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LS_Shop.Infrastructure
+{
+    interface ISessionManager
+    { 
+        //pobranie elementu
+        T Get<T>(string key);
+        //ustawienie elementu
+        void Set<T>(String name, T value);
+        // usuwanie elementu
+        void Abandon();
+        T TryGet<T>(string key);
+    }
+}
---
+++ LS-Shop/Infrastructure/SessionManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace LS_Shop.Infrastructure
+{
+    public class SessionManager : ISessionManager
+    {
+        private HttpSessionState session;
+        public SessionManager()
+        {
+            session = HttpContext.Current.Session;
+        }
+
+        //usuwanie sesji
+        public void Abandon()
+        {
+            session.Abandon();
+        }
+
+        //Zwracanie sesji
+        public T Get<T>(string key)
+        {
+            return (T)session[key];
+        }
+
+        //przypisanie wartosci sesji
+        public void Set<T>(string name, T value)
+        {
+            session[name] = value;
+        }
+
+        public T TryGet<T>(string key)
+        {
+            try
+            {
+                return (T)session[key];
+            }
+            catch (NullReferenceException)
+            {
+                return default(T);
+            }
+        }
+    }
+}
