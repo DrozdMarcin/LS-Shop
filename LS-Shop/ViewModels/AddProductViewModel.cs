c46ec675ca74e2e313ec48a511813535f1dd0811
Milosz Kulesza <milosz.kulesza1@gmail.com>
2019-05-26 22:23:00 +0200

    poprawa viewmodelu

--- LS-Shop/ViewModels/AddProductViewModel.cs
+++ LS-Shop/ViewModels/AddProductViewModel.cs
@@ -10,11 +10,7 @@ namespace LS_Shop.ViewModels
 {
     public class AddProductViewModel
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public bool Bestseller { get; set; }
-        public Category Category { get; set; }
+        public Product Product { get; set; }
         public HttpPostedFileBase NewFile { get; set; }
         public IEnumerable<Category> Categories { get; set; }
     }
